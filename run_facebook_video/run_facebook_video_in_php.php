580d4f35531598b60e545352d0e9f954a499e93e
zakir7dipu <66817549+zakir7dipu@users.noreply.github.com>
2021-07-05 12:35:12 +0600

    Update run_facebook_video_in_php.php

--- run_facebook_video/run_facebook_video_in_php.php
+++ run_facebook_video/run_facebook_video_in_php.php
@@ -8,12 +8,12 @@
     <title>Play facebook video in iframe dynamically</title>
 </head>
 <body>
-<h1 style="text-align: center">To run facebook video in iframe dynamicly We should filter the link url as like</h1>
+<h1 style="text-align: center">To run facebook video in iframe dynamically We should filter the link url as like</h1>
 <?php
 $prefix = 'https://www.facebook.com/plugins/video.php?height=314&href=';
 $safix = '&show_text=false&width=560&t=0';
 $request_video_url = 'https://www.facebook.com/DailyProthomAlo/videos/3544996325603356';
-$input_video = str_replace([':','/','?sfnsn=wa'],['%3A','%2F',''],$request_video_url);
+$input_video = str_replace(':','%3A',$request_video_url);
 $input_video = str_replace('/','%2F',$input_video);
 $input_video = str_replace('?sfnsn=wa','',$input_video);
 $video = $prefix.$input_video.$safix;
